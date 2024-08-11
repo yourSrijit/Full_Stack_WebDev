e6e87b212c52dded4deb61c9f696eac0eee62539
yourSrijit <berasrijit02@gmail.com>
2024-08-11 11:42:17 +0530

    Page created

--- 7_Node JS +Projects/Projects/Advanced OTP Verfication System/frontend/src/App.jsx
+++ 7_Node JS +Projects/Projects/Advanced OTP Verfication System/frontend/src/App.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import FloatingShape from './components/FloatingShape'
+import { Route, Routes } from 'react-router-dom'
+import SignupPage from './pages/SignupPage'
+import LoginPage from './pages/LoginPage'
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
-    <div className='min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden'>App</div>
+    <div className='min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden'>
+      <FloatingShape color='bg-green-500' size='w-64 h-64' top='-5%' left='10%' delay={0}/>
+      <FloatingShape color='bg-green-500' size='w-48 h-48' top='40%' left='80%' delay={5}/>
+      <FloatingShape color='bg-lime-500' size='w-32 h-32' top='40%' left='-5%' delay={2}/>
+
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/signup' element={<SignupPage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+      </Routes>
+    </div>
   )
 }
 
