c87bf3cf2027aaa9321270bc79deff1234df5ba2
yourSrijit <berasrijit02@gmail.com>
2024-09-01 20:37:42 +0530

    Update

--- 8_Next JS + Projects/Jonas Schmedtmann/The Wild oasis website/app/page.js
+++ 8_Next JS + Projects/Jonas Schmedtmann/The Wild oasis website/app/page.js
@@ -4,8 +4,7 @@ import Link from "next/link"
 export default function Page() {
   return (
     <div>
-     
-     <h1>The Wild Oasis.Welcome to paradice</h1>
+     <h1>ITC Royal Bengal.Welcome to paradice</h1>
    </div>
   );
 }
---
+++ 8_Next JS + Projects/Sangam Mukherjee/basics/src/app/products/[...detail]/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>This extra dynamic page</div>
+  )
+}
+
+export default page
