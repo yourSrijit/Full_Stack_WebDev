e46e6ca5b732093fa9e3ea07f239c4c3f1287ca4
yourSrijit <berasrijit02@gmail.com>
2024-09-10 21:04:23 +0530

    Link and useRouter usages

--- 8_Next JS + Projects/Sangam Mukherjee/basics/src/app/page.js
+++ 8_Next JS + Projects/Sangam Mukherjee/basics/src/app/page.js
@@ -1,7 +1,14 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+
 export default function Home() {
+  const router=useRouter();
   return (
    <div>
     Hi Srijit
+    <Link href={"/products"}>Product</Link>
+     <button onClick={()=> router.push("account")}>Go to account</button>
    </div>
   );
 }
